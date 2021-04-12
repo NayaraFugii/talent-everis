7973e8d8ec39a9892e39c282828174e63556669a
Nayara Fugii <nayarac.neto@gmail.com>
2021-04-12 15:35:27 -0300

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDl8VjUkZKWPKbPjvz5KZEU_zvuruJCljQ",
+    authDomain: "crow-d1704.firebaseapp.com",
+    projectId: "crow-d1704",
+    storageBucket: "crow-d1704.appspot.com",
+    messagingSenderId: "1095838761783",
+    appId: "1:1095838761783:web:23e68ec3352c31bf2a10b2",
+    measurementId: "G-LD3MN9J3NK"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
