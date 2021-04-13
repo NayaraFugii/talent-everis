9ebd45245a97ee531774dd5ebca8ba66d4246ac2
KarineFrontelli <you@example.comkarinefrontellidossantos@gmail.com>
2021-04-12 23:03:25 -0300

    Criação das Rotas

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+import Feed from './Feed'
+import Profile from './Profile'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import HomeApp from './HomeApp';
 import Login from './Login'
 import Register from './Register'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HomeApp />
-    <Login/>
-    <Register/>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact={true} component={HomeApp} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/feed" component={Feed} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
