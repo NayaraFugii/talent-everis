e7cc494825d08b947f9d68f33aa8baf95394df3a
Tanagra Andria <design.andria@gmail.com>
2021-04-13 22:04:32 -0300

    CSS Profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,47 @@
 import React from 'react'
+import './style/Profile.css'
+import ButtonApp from './components/Button'
+import { useHistory } from 'react-router-dom'
+import firebase  from './firebase'
+import crow from './img/crow.png'
+
 
 function Profile() {
+  const history = useHistory();
+
+  const routerHome = () => {
+    history.push('/')
+}
+  const logout = async(e)=>{
+    e.preventDefault() 
+    localStorage.removeItem("uid");
+    await firebase.auth().signOut().then(() => {
+       routerHome()
+
+     })
+
+  }  
 
 
   return (
     <>
+    <div className="flexContainer">
+      <header className="header">
+        <img src={crow} alt="" className="logoCrow"/>
+      </header>
+      <section className="template">
+      <div className="cover"></div>
+      <div className="ProfileArea"></div>
       <h1>Profile</h1>
-
+      </section>
+      <div>
+        <ButtonApp
+          buttonOnClick = {logout}
+          buttonText="Sair"
+          btnClassName="btnExit"
+        />
+      </div>
+    </div>
     </>
   );
 }
